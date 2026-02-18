891235e2523be7ac186fa61bc98d5a88f08cad1a
Oskar Tegby <oskar.tegby@proton.me>
2026-02-18 22:40:15 +0100

    [Feature] Added alias for the bastion at the University of Murcia

--- ssh-cmds.sh
+++ ssh-cmds.sh
@@ -1,4 +1,5 @@
 alias mn="ssh bsc003114@glogin1.bsc.es"
 alias mn2="ssh bsc003114@glogin2.bsc.es"
+alias ab="ssh otegby@abdet.inf.um.es"
 
 alias rsc="rsync -avz --progress src inc scripts bsc003114@glogin1.bsc.es:/home/bsc/bsc003114/ChampSim-dev"
